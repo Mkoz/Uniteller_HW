e1a2879e9a3ce429c01abcedf73a206073abb06a
Mkoz <mefistofel_89@list.ru>
2022-06-13 18:42:06 +0300

    Cosmetic change

--- main.cpp
+++ main.cpp
@@ -2,32 +2,5 @@
 #include "Indx_impl/Indx.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    auto el = El();
-
-    std::cout << ((int)El::_end_num-(int)El::_start_num) << std::endl;
-
-    for(auto i = 0; i < ((int)El::_end_num-(int)El::_start_num + 1)* \
-                        ((int)El::_end_liter - (int)El::_start_liter - \
-                        sizeof(El::_except_arr) / sizeof(*El::_except_arr) + 1) -1; i++)
-    {
-        el.plus(1);
-        std::cout << el.get() << " ";
-    }
-    std::cout << std::endl;
-    el.set("A2");
-
-
-    auto ind = Indx();
-    std::cout << ind.get().size() << std::endl;
-
-    ind.plus(1);
-    std::cout << ind.get().size() << std::endl;
-
-
-    for(auto str : ind.get())
-    {
-        std::cout << str.get();
-    }
     return 0;
 }
